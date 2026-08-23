a02ba4a80029b35829576e2baa57bc76b07d3e14
JBirnbickl93 <jo.birnbickl@web.de>
2026-08-23 21:56:32 +0200

    Add proxy-config and observable login method

--- angular/src/app/User/Login/LoginForm/login-form.component.ts
+++ angular/src/app/User/Login/LoginForm/login-form.component.ts
@@ -1,8 +1,9 @@
-import {Component, Input} from '@angular/core';
+import {Component} from '@angular/core';
 import {MatFormField, MatInputModule} from '@angular/material/input';
 import {Router} from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
 import {FormsModule} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-login-component',
@@ -17,14 +18,31 @@ import {FormsModule} from '@angular/forms';
 })
 export class LoginFormComponent {
 
-  constructor(private router: Router) { }
-  username: string = " ";
-  password: string = " ";
+  constructor(private router: Router,
+              private http: HttpClient) {
+  }
+
+  email: string = '';
+  password: string = '';
 
   protected login(): void {
-    console.log(this.username);
-    console.log(this.password);
-    this.router.navigate(['/dashboard']);
+    this.http.post<{ token: string }>(
+      '/api/auth/login',
+      {
+        email: this.email,
+        password: this.password
+      }
+    ).subscribe({
+      next: response => {
+        console.log('Login successful');
+        console.log(response.token);
+        this.router.navigate(['/dashboard']);
+      },
+      error: error => {
+        console.log('Login failed.', error);
+      }
+    })
+
   }
-  // TODO: EventEmitter for login action
 }
+  // TODO: EventEmitter for login action
--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LoginFormComponent } from './User/Login/LoginForm/login-form.component'
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
   { path: 'login', component: LoginFormComponent },
   { path: 'dashboard', component: DashboardComponent },
 ];
