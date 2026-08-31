abda2ef31796a39cdbeda8272d3b6bdb94df5cda
JBirnbickl93 <jo.birnbickl@web.de>
2026-08-31 21:09:29 +0200

    Add more tests to AuthService.ts

--- angular/src/app/User/auth/auth.service.spec.ts
+++ angular/src/app/User/auth/auth.service.spec.ts
@@ -3,25 +3,38 @@ import { TestBed } from '@angular/core/testing';
 import { AuthService } from './auth.service';
 import {LoginFormComponent} from '../login/login/login-form.component';
 import {provideHttpClient} from '@angular/common/http';
-import {provideHttpClientTesting} from '@angular/common/http/testing';
+import {HttpTestingController, provideHttpClientTesting} from '@angular/common/http/testing';
 
 describe('AuthService', () => {
-  let service: AuthService;
-
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [LoginFormComponent],
       providers: [provideHttpClient(),
-      provideHttpClientTesting()]
+        provideHttpClientTesting()]
     });
     service = TestBed.inject(AuthService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  let service: AuthService = TestBed.inject(AuthService);
+
+  service.saveToken('abc123');
+  expect(service.getToken()).toBe('abc123');
+
+  service.login('test@test.de', 'test').subscribe((response) => {
+    expect(response.token).toBe('abc123');
   });
 
-  it('should return a token after login', () => {
-    expect(localStorage.getItem('token')).toBeTruthy();
-  })
-});
+  const httpMock = TestBed.inject(HttpTestingController);
+  const request = httpMock.expectOne('/api/auth/login');
+
+  request.flush({
+    token: 'abc123'
+  });
+
+  expect(request.request.method).toBe('POST');
+  expect(request.request.body).toEqual({
+    email: 'test@test.de',
+    password: 'test'
+  });
+
+})
