26aef8798cbbff362fba0dbcd51143056dcc17ea
JBirnbickl93 <jo.birnbickl@web.de>
2026-08-31 20:48:15 +0200

    Remove default tests and implement first tests for AuthService

--- angular/src/app/User/auth/auth.service.spec.ts
+++ angular/src/app/User/auth/auth.service.spec.ts
@@ -1,16 +1,27 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth.service';
+import {LoginFormComponent} from '../login/login/login-form.component';
+import {provideHttpClient} from '@angular/common/http';
+import {provideHttpClientTesting} from '@angular/common/http/testing';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [LoginFormComponent],
+      providers: [provideHttpClient(),
+      provideHttpClientTesting()]
+    });
     service = TestBed.inject(AuthService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return a token after login', () => {
+    expect(localStorage.getItem('token')).toBeTruthy();
+  })
 });
