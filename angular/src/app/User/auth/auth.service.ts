5e15441f2f88825b440cfdde457c017b491af8ff
JBirnbickl93 <jo.birnbickl@web.de>
2026-08-31 21:20:42 +0200

    Fix Bug in Test URL

--- angular/src/app/User/auth/auth.service.ts
+++ angular/src/app/User/auth/auth.service.ts
@@ -6,7 +6,7 @@ import {shareReplay} from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'api/auth'
+  private apiUrl = '/api/auth'
   constructor(private http: HttpClient) {
   }
 
